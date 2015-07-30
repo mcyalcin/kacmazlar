332b50aa9c80eefad194b11a593867a1f15b695e
Mehmet Cihan Yalcin <mcyalcin@gmail.com>
2015-07-30 13:28:32 +0300

    crud frontend done

--- public/javascripts/drivers.js
+++ public/javascripts/drivers.js
@@ -1,14 +1,58 @@
-$(document).ready(function() {
-  $('#drivers').dataTable( {
-    "ajax": "drivers/api",
-    "columns": [
-      { "data": "name" },
-      { "data": "surname" },
-      { "data": "id_number" },
-      { "data": "mother_name" },
-      { "data": "father_name" },
-      { "data": "birthplace" },
-      { "data": "phone_number" }
+var editor;
+
+$(document).ready(function () {
+  editor = new $.fn.dataTable.Editor({
+    ajax: "drivers/api",
+    table: "#drivers",
+    fields: [{
+      label: "Adı:",
+      name: "name"
+    }, {
+      label: "Soyadı:",
+      name: "surname"
+    }, {
+      label: "TC Kimlik Numarası:",
+      name: "id_number"
+    }, {
+      label: "Ana adı:",
+      name: "mother_name"
+    }, {
+      label: "Baba adı:",
+      name: "father_name"
+    }, {
+      label: "Doğum Yeri:",
+      name: "birthplace"
+    }, {
+      label: "Telefon Numarası:",
+      name: "phone_number"
+    }
+      //,  {
+      //  label: "Doğum Yeri:",
+      //  name: "birthplace",
+      //  type: "date"
+      //}
     ]
-  } );
+  });
+
+  $('#drivers').dataTable({
+    dom: "Tfrtip",
+    ajax: "drivers/api",
+    columns: [
+      {"data": "name"},
+      {"data": "surname"},
+      {"data": "id_number"},
+      {"data": "mother_name"},
+      {"data": "father_name"},
+      {"data": "birthplace"},
+      {"data": "phone_number"}
+    ],
+    tableTools: {
+      sRowSelect: "os",
+      aButtons: [
+        {sExtends: "editor_create", editor: editor},
+        {sExtends: "editor_edit", editor: editor},
+        {sExtends: "editor_remove", editor: editor}
+      ]
+    }
+  });
 });
