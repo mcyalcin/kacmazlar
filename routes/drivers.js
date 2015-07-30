f00c7dac5bf30344a4c9ec4ad60294eb35b4f487
Mehmet Cihan Yalcin <mcyalcin@gmail.com>
2015-07-30 17:28:29 +0300

    template->ejs, navbar prototype

--- routes/drivers.js
+++ routes/drivers.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views', 'drivers.html'));
+  //res.sendFile(path.join(__dirname, '../views', 'drivers.html'));
+  res.render('drivers', {});
 });
 
 module.exports = router;
