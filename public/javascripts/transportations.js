1a819a0133496ce8b96704c67d15f21823f43c2f
Mehmet Cihan Yalcin <mcyalcin@gmail.com>
2015-08-03 12:17:16 +0300

    transportations placeholder done

--- public/javascripts/transportations.js
+++ public/javascripts/transportations.js
@@ -1,3 +1,35 @@
-/**
- * Created by mcyalcin on 03.08.2015.
- */
+$(document).ready(function() {
+  $('#example').DataTable({
+    "ajax": "data/tableData.json",
+    dom: 'T<"clear">lfrtip',
+    "scrollX": true,
+    "columns": [
+      { "data": "yukleme_tarihi" },
+      { "data": "bosaltma_tarihi" },
+      { "data": "firma_adi" },
+      { "data": "cekici_plakasi" },
+      { "data": "dorse_plakasi" },
+      { "data": "sofor_adi_soyadi" },
+      { "data": "yukleme_yeri" },
+      { "data": "bosaltim_yeri" },
+      { "data": "cmr_tarihi" },
+      { "data": "cmr_no" },
+      { "data": "urun_cinsi" },
+      { "data": "yukleme_tonaji" },
+      { "data": "gumruk_tonaji" },
+      { "data": "bosaltim_tonaji" },
+      { "data": "fire_hakki" },
+      { "data": "gumruk_firesi" },
+      { "data": "urun_firesi" },
+      { "data": "gumruk_fire_fiyati" },
+      { "data": "urun_fire_fiyati" },
+      { "data": "gumruk_cezasi" },
+      { "data": "urun_cezasi" },
+      { "data": "cmr_bedeli" },
+      { "data": "nakliye_fiyati" },
+      { "data": "nakliye_tutari" },
+      { "data": "net_hakedis_tutari" },
+      { "data": "hakedis_tarihi" }
+    ]
+  });
+});
